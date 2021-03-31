1535da3a6b5414aa00d94657dfae7d4beb14ced5
DHukov <48653308+DHukov@users.noreply.github.com>
2021-03-31 11:48:06 +0200

    Last changes
    New scene

--- Sanguis-Universum/Assets/Scripts/PlayerController.cs
+++ Sanguis-Universum/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
+//using UnityEngine.Rendering.PostProcessing;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] PostProcessVolume postProcess;
+   // [SerializeField] PostProcessVolume postProcess;
     [SerializeField] SpriteRenderer swordFire;
     private Rigidbody2D _rigidbody;
-    private Collider2D _collider;
-    private Animator _animator;
+    private CapsuleCollider2D _collider;
+   // private Animator _animator;
     public LayerMask layermask;
 
     public float speed = 4f;
     public float jump = 8f;
     bool swordInFire = false;
-   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("It is Work!");
+    }
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
-        _animator = GetComponent<Animator>();
+        _collider = GetComponent<CapsuleCollider2D>();
+     //   _animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,7 +34,8 @@ public class PlayerController : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.C))
         {
-            _animator.Play("Attack");
+            //     _animator.Play("Attack");
+            Debug.Log("dsa");
             
         }
 
@@ -38,42 +43,44 @@ public class PlayerController : MonoBehaviour
         {
             _rigidbody.velocity = new Vector2(-speed, _rigidbody.velocity.y);
             transform.localScale = new Vector3(-1f , 1f);
-            _animator.SetBool("walking" , true);
+        //    _animator.SetBool("walking" , true);
         }
         else if(direction > 0.0f)
         {
             _rigidbody.velocity = new Vector2(speed, _rigidbody.velocity.y);
             transform.localScale = new Vector3(1f , 1f);
-            _animator.SetBool("walking", true);
+         //   _animator.SetBool("walking", true);
         }
         else
         {
-            _animator.SetBool("walking", false);
+         //   _animator.SetBool("walking", false);
             _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && _collider.IsTouchingLayers(layermask))
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jump);
-            _animator.Play("Jump");
+        //    _animator.Play("Jump");
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if(!swordInFire)
             {
+
                 swordInFire = true;
-                postProcess.weight = 0.5f;
+                //postProcess.weight = 0.5f;
                 swordFire.enabled = true;
             }
             else
             {
                 swordInFire = false;
-                postProcess.weight = 1f;
+                //postProcess.weight = 1f;
                 swordFire.enabled = false;
             }
 
         }
+
     }
 
     
