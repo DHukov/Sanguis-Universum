f927dae63d1b138af8b1192748a27589052c827a
Jakubl14 <56953021+Jakubl14@users.noreply.github.com>
2021-04-30 03:40:36 +0200

    + crouching (beta)

--- Sanguis-Universum/Assets/Scripts/PlayerScripts/CharController.cs
+++ Sanguis-Universum/Assets/Scripts/PlayerScripts/CharController.cs
@@ -11,9 +11,12 @@ public class CharController : MonoBehaviour
 
     public Transform GroundCheck;
     public LayerMask m_WhatIsGround;
+    public Transform CeilingCheck;
+    public Collider2D CrouchDisableCollider;
 
     public float m_JumpForce = 400f;
     const float k_GroundedRadius = .05f;
+    const float k_CeilingRadius = .2f;
     private bool m_Grounded;
     private bool m_FacingRight = true;
     private Vector3 m_Velocity = Vector3.zero;
@@ -23,6 +26,7 @@ public class CharController : MonoBehaviour
     bool jump = false;
     float h_Move = 0f;
     public float runSpeed = 40f;
+    public float crouchSpeed = 20f;
 
     public UnityEvent OnLandEvent;
     public VectorValue PlayerPosition;
@@ -37,6 +41,10 @@ public class CharController : MonoBehaviour
 
     }
 
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
+
+
     void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
@@ -45,12 +53,55 @@ public class CharController : MonoBehaviour
         {
             OnLandEvent = new UnityEvent();
         }
+
+        if (OnCrouchEvent == null)
+        {
+            OnCrouchEvent = new BoolEvent();
+        }
     }
 
-    void Move(float move, bool jump)
+    void Move(float move, bool jump, bool crouch)
     {
+
+        /*if (!crouch)
+        {
+            if (Physics2D.OverlapCircle(CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            {
+                crouch = true;
+            }
+        }*/
+
         if (m_Grounded || !m_Grounded)
         {
+
+            /*if (crouch)
+            {
+                if (!m_wasCrouching)
+                {
+                    m_wasCrouching = true;
+                    OnCrouchEvent.Invoke(true);
+                }
+
+                
+                move *= crouchSpeed;
+
+                
+                if (CrouchDisableCollider != null)
+                    CrouchDisableCollider.enabled = false;
+            }
+            else
+            {
+                
+                if (CrouchDisableCollider != null)
+                    CrouchDisableCollider.enabled = true;
+
+                if (m_wasCrouching)
+                {
+                    m_wasCrouching = false;
+                    OnCrouchEvent.Invoke(false);
+                }
+            }*/
+
             Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
 
             m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmooth);
